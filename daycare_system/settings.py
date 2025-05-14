bc9c2727a8ff1c85f9b9913ddded0a299d12fc73
Eousuf Siddiquee <eousuf001siddiquee@gmail.com>
2025-05-14 10:26:06 +0600

    Configure static files with WhiteNoise for Render deployment

--- daycare_system/settings.py
+++ daycare_system/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -144,3 +147,5 @@ AUTHENTICATION_BACKENDS = [
 LOGIN_REDIRECT_URL = 'index'  # After successful login
 LOGOUT_REDIRECT_URL = 'login'  # After logout
 LOGIN_URL = 'login'  # Where to redirect for login required
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
