a5789bfe3eced8e3aee114942dce07814ca3f029
Eousuf Siddiquee <eousuf001siddiquee@gmail.com>
2025-05-14 10:00:37 +0600

    Initial commit for Render deployment

--- daycare_system/settings.py
+++ daycare_system/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure--aczzvtwhbs3z=uf#oos^n@s^d15$d6k*-#iahy0%lu%&&m3_u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
